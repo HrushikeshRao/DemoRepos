4e61fffdd640966af608114a9411a90d8f284b1f
HrushikeshRao <hrushikeshrao9148717557@gmail.com>
2024-04-12 06:20:13 +0100

    Try catch added to the connection failure exception.

---
+++ Mail_User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MailBoxEmpty01
+{
+    public partial class Mail_User : Form
+    {
+        public Mail_User()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+    }
+}
